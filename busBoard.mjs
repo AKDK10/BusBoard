6739669ec60f45b551dd6991bfa12f2130675d43
Akla Djankla <djsolangea@gmail.com>
2023-07-07 14:47:42 +0100

    added function findCoordinate

--- busBoard.mjs
+++ busBoard.mjs
@@ -1,22 +1,49 @@
-import fetch from "node-fetch";
+// Import the readline module
 
-async function busBoard(){
+//import * as readline from "readline";
 
+//const readline = require('readline-sync');
 
+// import fetch from "node-fetch";
+
+// async function busBoard(){
    
-    const busStopCode = "490008660N"; 
-    const fetchArrivalData = await fetch(`https://api.tfl.gov.uk/StopPoint/${busStopCode}/Arrivals`);
-    const busArrivalInfo = await fetchArrivalData.json();
+//     const busStopCode = "490008660N"; 
+//     const fetchArrivalData = await fetch(`https://api.tfl.gov.uk/StopPoint/${busStopCode}/Arrivals`);
+//     const busArrivalInfo = await fetchArrivalData.json();
 
-    //console.log(busArrivalInfo)
+//     //console.log(busArrivalInfo)
 
 
-    for(let i = 0; i < busArrivalInfo.length; i++){
+//     for(let i = 0; i < busArrivalInfo.length; i++){
 
-        const NextBusInfo = busArrivalInfo[i];
+//         const NextBusInfo = busArrivalInfo[i];
 
-        console.log(`Next bus arriving is ${NextBusInfo.lineName} in ${NextBusInfo.timeToStation} seconds `)
-    }
+//         console.log(`Next bus arriving is ${NextBusInfo.lineName} in ${NextBusInfo.timeToStation} seconds `)
+//     }
+// }
+
+// busBoard()
+
+// find longitude and latitude
+let userPostCode ="n225na";
+let searchRadius = "500";
+
+async function findCoordinate(userPostCode){
+    
+    const postCodeResponse = await fetch(`https://api.postcodes.io/postcodes/${userPostCode}`);
+    const coordData = await postCodeResponse.json();
+
+    // const userLon = coordData.result.longitude;
+    // const userLat = coordData.result.latitude;
+    const retrievedCoords = {
+        userLon: coordData.result.longitude,
+        userLat: coordData.result.latitude,
+    };
+    return retrievedCoords;
+    
 }
 
-busBoard()
+const userCoords = findCoordinate(userPostCode);
+
+console.log(userCoords);
